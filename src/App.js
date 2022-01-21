1a98300c90503f28e64d36474df8dcfd495a0741
이상철 <zlrz6706@gmail.com>
2022-01-21 18:28:16 +0900

    2022-01-21

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Counter from "./Counter";
-import EventPractice from "./EventPractice";
-import EventPracticeFunc from "./EventPracticeFunc";
-import MyComponent from "./MyComponent";
-import Say from "./Say";
-import LoginPractice from "./LoginPractice";
+import Validation from "./Validation";
 
 function App() {
   return (
     <>
-    <LoginPractice />
+    <Validation />
     </>
   );
 }
--- src/EventPracticeFunc.js
+++ src/EventPracticeFunc.js
@@ -1,67 +1,56 @@
 import React,{useState} from 'react'
 
-
 const EventPracticeFunc = () => {
 
-  const [form, setForm] = useState({
-    username : '',
-    message: '',
+  const [text, setText] = useState({
+    id:"",
+    password:""
   })
 
-  const {username, message} = form
+  const {id, password} = text;
 
-  const onChange = e => {
-    const nextForm = {
-      ...form,
+  const onChange = (e) => {
+    const newText = { 
+      ...text,
       [e.target.name] : e.target.value
     }
-    setForm(nextForm)
-  };
-  
-  const onClick = () => {
-    alert(username + ": "+ message);
-    setForm({
-      username: '',
-      message: ''
-    });
-  };
-
-  const onKeyPress = e => {
-    if(e.key === "Enter") {
-      onClick()
-    }
-    
+    setText(newText);
   }
 
+  const onClick = () => {
+    alert(`id는 ${id}이고, password는 ${password}입니다.`)
+    setText({
+      id:'',
+      password:''
+    })
+    console.log("초기화면입니다.")
+  }
 
   return (
     <>
-      <h1>이벤트 연습</h1>
-      <input 
-        type="text"
-        name="username"
-        placeholder="username"
-        value={username}
-        onChange={onChange}
-      />
+    <input 
+      type="text"
+      name="id"
+      placeholder="id"
+      value={id}
+      onChange={onChange}
+    />
+    
+    <input 
+      type="password"
+      name="password"
+      placeholder="password"
+      value={password}
+      onChange={onChange}
 
-      <input 
-        type="text"
-        name="message"
-        placeholder="message"
-        value={message}
-        onChange={onChange}
-        onKeyPress={onKeyPress}
-      />
+    />
 
-      <button 
-        onClick={onClick}
-      >go!</button>
-
+    <button
+      onClick={onClick}
+    >Yes</button>
     </>
 
   )
-
 }
 
 export default EventPracticeFunc
