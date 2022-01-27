7fe3e4d352d84c05814553e0b5785376f35b071c
sangcheol LEE <zlrz6706@gmail.com>
2022-01-27 12:33:54 +0900

    usememo basic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React,{Component, useEffect, useState} from "react";
-import Counter from "./Counter";
-import Info from "./Info";
+import Average from "./Average";
 
 
 const App = () => {
 
-  const [visible, setVisible] = useState(false)
 
   
   
   return (
     <>
-    <button
-      onClick={() => setVisible(!visible)}
-    >{visible ? "숨기기" : "보이기"}</button>
-    {visible && <Info/>}
-
-    <Counter />
+    <Average />
     </>
   );
 
