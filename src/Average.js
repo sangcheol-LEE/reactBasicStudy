91bb429947b4ef98ff65dcab5bbdcea815493f41
sangcheol LEE <zlrz6706@gmail.com>
2022-02-03 11:16:22 +0900

    HOOK기본

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React,{ useState, useMemo } from "react";
+import React,{ useState, useMemo, useCallback } from "react";
 
 const getAverage = numbers => {
     console.log('계산 중...')
@@ -12,14 +12,15 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
+    const onChange = useCallback(e  => {
         setNumber(e.target.value);
-    }
-    const onInsert = e => {
+    },[]) // 컴포넌트가 처음 렌더링 될 때만 함수 생성
+
+    const onInsert = useCallback(()=> {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    }
+    },[number, list]) // number 혹은 list가 바뀌었을 때만 함수생성
 
     const avg = useMemo(()=> getAverage(list),[list])
     
